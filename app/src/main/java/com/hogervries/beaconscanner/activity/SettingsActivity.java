9fa46cd33957bb064ca023230cff5dc15b9a9b56
Boyd Hogerheijde <boydhogerheijde@gmail.com>
2016-03-15 15:44:51 +0100

    add to fragmentmanager instead of replace

--- app/src/main/java/com/hogervries/beaconscanner/activity/SettingsActivity.java
+++ app/src/main/java/com/hogervries/beaconscanner/activity/SettingsActivity.java
@@ -13,7 +13,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+        getFragmentManager().beginTransaction().add(android.R.id.content, new SettingsFragment()).commit();
     }
 
     @Override
