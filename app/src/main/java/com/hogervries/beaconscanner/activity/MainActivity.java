6290399ca1895cbe04ec8acac61b46d3a9f0f522
Boyd Hogerheijde <boydhogerheijde@gmail.com>
2016-05-19 21:58:14 +0200

    Added singlefragment activity

--- app/src/main/java/com/hogervries/beaconscanner/activity/MainActivity.java
+++ app/src/main/java/com/hogervries/beaconscanner/activity/MainActivity.java
@@ -1,11 +1,8 @@
 package com.hogervries.beaconscanner.activity;
 
-import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 
-import com.hogervries.beaconscanner.R;
 import com.hogervries.beaconscanner.fragment.MainFragment;
 
 /**
@@ -14,24 +11,12 @@ import com.hogervries.beaconscanner.fragment.MainFragment;
  * @author Boyd Hogerheijde
  * @author Mitchell de Vries
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
-    private static final String DIALOG_BEACON = "dialog_beacon";
-
+    @NonNull
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-        // Gets fragment manager and current fragment in layout.
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
-        // Check if the fragment exists, if not it adds a new one.
-        if (fragment == null) {
-            fragment = MainFragment.newInstance();
-            fragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return MainFragment.newInstance();
     }
 
 }
---
+++ app/src/main/java/com/hogervries/beaconscanner/activity/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.hogervries.beaconscanner.activity;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hogervries.beaconscanner.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    @NonNull
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
