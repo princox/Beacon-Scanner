d7b9d4b196a25addc33149f8b5b404e9dfc1f8cc
Mitchell de Vries <mdvries1@live.nl>
2016-05-20 18:19:12 +0200

    Added detail view

---
+++ app/src/main/java/com/hogervries/beaconscanner/activity/BeaconDetailActivity.java
@@ -0,0 +1,22 @@
+package com.hogervries.beaconscanner.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+import com.hogervries.beaconscanner.fragment.BeaconDetailFragment;
+
+/**
+ * Trifork Netherlands.
+ * GuestApp Social.
+ * Mitchell de Vries & Mohammed Ali.
+ */
+
+public class BeaconDetailActivity extends SingleFragmentActivity {
+
+    @NonNull
+    @Override
+    protected Fragment createFragment() {
+        return BeaconDetailFragment.newInstance();
+    }
+}
