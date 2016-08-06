c7c6d5accb9d22c9a374b4f8573e95ceb5b85a3d
Mitchell de Vries <mdvries1@live.nl>
2016-08-06 18:38:47 +0200

    Gitkraken failed to push these 3 files, see last commit

--- app/src/main/java/com/hogervries/beaconscanner/activity/SettingsActivity.java
+++ app/src/main/java/com/hogervries/beaconscanner/activity/SettingsActivity.java
@@ -1,76 +1,25 @@
 package com.hogervries.beaconscanner.activity;
 
-
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import com.hogervries.beaconscanner.R;
+import com.hogervries.beaconscanner.fragment.SettingsFragment;
 
-public class SettingsActivity extends PreferenceActivity {
-
-    private static Preference.OnPreferenceChangeListener bindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-
-            if (preference instanceof ListPreference) {
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
-                preference.setSummary(
-                        index >= 0 ? listPreference.getEntries()[index] : null);
-
-            } else {
-                preference.setSummary(stringValue);
-            }
-            return true;
-        }
-    };
-
-    private static void bindPreferenceSummaryToValue(Preference preference) {
-        preference.setOnPreferenceChangeListener(bindPreferenceSummaryToValueListener);
-
-        bindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
-    }
+public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
-
+        setupActionBar();
     }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-            setHasOptionsMenu(true);
-
-            bindPreferenceSummaryToValue(findPreference("preference_tracking_age"));
-            bindPreferenceSummaryToValue(findPreference("preference_scan_frequency"));
-            bindPreferenceSummaryToValue(findPreference("preference_between_scan_period"));
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
--- app/src/main/java/com/hogervries/beaconscanner/fragment/SettingsFragment.java
+++ app/src/main/java/com/hogervries/beaconscanner/fragment/SettingsFragment.java
@@ -1,30 +1,77 @@
 package com.hogervries.beaconscanner.fragment;
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-            setHasOptionsMenu(true);
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-            bindPreferenceSummaryToValue(findPreference("preference_tracking_age"));
-            bindPreferenceSummaryToValue(findPreference("preference_scan_frequency"));
-            bindPreferenceSummaryToValue(findPreference("preference_between_scan_period"));
-            bindPreferenceSummaryToValue(findPreference("preference_uuid"));
-            bindPreferenceSummaryToValue(findPreference("preference_major"));
-            bindPreferenceSummaryToValue(findPreference("preference_minor"));
-            bindPreferenceSummaryToValue(findPreference("preference_tx_power"));
-            bindPreferenceSummaryToValue(findPreference("preference_log_max_lines"));
-        }
+import com.hogervries.beaconscanner.R;
+import com.hogervries.beaconscanner.activity.SettingsActivity;
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        setHasOptionsMenu(true);
+
+        bindPreferenceSummaryToValue(findPreference("preference_tracking_age"));
+        bindPreferenceSummaryToValue(findPreference("preference_scan_frequency"));
+        bindPreferenceSummaryToValue(findPreference("preference_between_scan_period"));
+        bindPreferenceSummaryToValue(findPreference("preference_uuid"));
+        bindPreferenceSummaryToValue(findPreference("preference_major"));
+        bindPreferenceSummaryToValue(findPreference("preference_minor"));
+        bindPreferenceSummaryToValue(findPreference("preference_tx_power"));
+        bindPreferenceSummaryToValue(findPreference("preference_log_max_lines"));
     }
+
+    private Preference.OnPreferenceChangeListener bindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                preference.setSummary(
+                        index >= 0 ? listPreference.getEntries()[index] : null);
+
+            } else {
+                if (preference.getKey().equals("preference_major") || preference.getKey().equals("preference_minor")) {
+                    if (Integer.parseInt(stringValue) > 65535) {
+                        Toast.makeText(getActivity(), "Invalid Value", Toast.LENGTH_SHORT).show();
+                    } else {
+                        preference.setSummary(stringValue);
+                    }
+                } else {
+                    preference.setSummary(stringValue);
+
+                }
+            }
+            return true;
+        }
+    };
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(bindPreferenceSummaryToValueListener);
+
+        bindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(getActivity(), SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
